93056da1d4ba9173e560ad59d28407909799f4ed
Bhumesh Domala <bhumesh@codeft.in>
2019-03-18 11:22:53 +0530

    add download.js

---
+++ download.js
@@ -0,0 +1,8 @@
+
+// dlAnchorElem = document.createElement("a");
+// dataStr =
+// 	"data:text/json;charset=utf-8," +
+// 	encodeURIComponent(JSON.stringify(problemsAndSolutions, null, 2));
+// dlAnchorElem.setAttribute("href", dataStr);
+// dlAnchorElem.setAttribute("download", "scene11.json");
+// dlAnchorElem.click();
--- index.js
+++ index.js
@@ -47,11 +47,3 @@ sectionsList.forEach(async url => {
 	section.problems = problemsList;
 	problemsAndSolutions.push(section);
 });
-
-// dlAnchorElem = document.createElement("a");
-// dataStr =
-// 	"data:text/json;charset=utf-8," +
-// 	encodeURIComponent(JSON.stringify(problemsAndSolutions, null, 2));
-// dlAnchorElem.setAttribute("href", dataStr);
-// dlAnchorElem.setAttribute("download", "scene11.json");
-// dlAnchorElem.click();
